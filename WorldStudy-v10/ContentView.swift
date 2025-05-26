82c1caa5010e2e17235175fdd6b3c496948921c5
seohallo <seoohaii@gmail.com>
2025-05-27 00:35:17 +0900

    Initial Commit

---
+++ WorldStudy-v10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldStudy-v10
+//
+//  Created by 이서하 on 5/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldStudy-v10/WorldStudy_v10App.swift
@@ -0,0 +1,17 @@
+//
+//  WorldStudy_v10App.swift
+//  WorldStudy-v10
+//
+//  Created by 이서하 on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WorldStudy_v10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
